5693d5858256ba450c576733fc64e5449b5903e4
Max Mulatz <klappradla@gmail.com>
2016-11-09 00:25:29 +0100

    Get weird with helpers :tada:

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,9 +1,22 @@
 module CustomHelpers
+  LINK_MATCHER = /\[.*?\]/
   def link_list(links)
-    output = []
-    links.each do |link|
-      output << link_to(link.txt, link.link, target: :_blank)
+    links = links.map { |txt, url| link_to(txt, url, target: :_blank) }
+    links.join(', ').html_safe
+  end
+
+  def auto_link(text, links)
+    text = text.gsub(/\[.*?\]/) do |link|
+      link_to(strip_txt(link), links[to_key(link)], target: :_blank)
     end
-    output.join(', ').html_safe
+    simple_format(text.html_safe)
+  end
+
+  def to_key(link_txt)
+    strip_txt(link_txt).downcase
+  end
+
+  def strip_txt(link_txt)
+    link_txt.delete('[]')
   end
 end
